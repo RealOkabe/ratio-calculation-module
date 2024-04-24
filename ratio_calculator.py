a754178728a7ee7427507c20e340082783ceddc9
RealOkabe <anmolzagrawal@gmail.com>
2024-04-23 23:06:44 -0400

    Added a function to calculate the pe ratio

--- ratio_calculator.py
+++ ratio_calculator.py
@@ -1,1 +1,4 @@
 class RatioCalculator:
+    # Calculate price-to-earnings ratio
+    def calculate_pe_ratio(price_per_share, earnings_per_share):
+        return price_per_share / earnings_per_share
