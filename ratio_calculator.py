b258283f22496bdb9e1bad2a2d56d871479d344b
RealOkabe <anmolzagrawal@gmail.com>
2024-04-28 18:17:20 -0400

    Added code to calculate a few important values
    Inclused pe ratio, price change percentage, volume-weighted average price and relative strength index.

--- ratio_calculator.py
+++ ratio_calculator.py
@@ -10,19 +10,39 @@ class RatioCalculator:
         self.end = end
 
     # Calculate price-to-earnings ratio
-    def calculate_pe_ratio(self, ticker: str, start: str, end: str) -> float:
-        ticker_data = yf.Ticker(self.ticker, start = self.start, end = self.end)
-        pe_ratio = ticker_data['Close'].iloc[-1] / ticker_data.info['trailingPE']
-        return pe_ratio
+    def calculate_pe_ratio(self, ticker: str, start: str, end: str):
+        ticker_data = yf.Ticker(self.ticker).history(start = self.start, end = self.end)
+        avg_price = (ticker_data['Open'] + ticker_data['High'] + ticker_data['Low'] + ticker_data['Close']) / 4
+        ticker_data['PE Ratio'] = avg_price / (ticker_data['Close'] - ticker_data['Open'])
+        return ticker_data
 
-    # Get the return on equity
-    def get_return_on_equity(self, ticker: str):
-        ticker_data = yf.Ticker(self.ticker)
-        return ticker_data['returnOnEquity']
+    # Calculate Price Change percentage
+    def calculate_pc_percent(self, ticker: str, start: str, end: str):
+        ticker_data = yf.Ticker(self.ticker).history(start = self.start, end = self.end)
+        book_price = ticker_data['Close'] - ticker_data['Open']
+        ticker_data['Price Change Percentage'] = book_price / ticker_data['Open']
+        return ticker_data
 
-    # Get the return on assets
-    def get_return_on_assets(self, ticker: str):
-        ticker_data = yf.Ticker(self.ticker)
-        return ticker_data['returnOnAssets']
+    # Calculate volume-weighted average price
+    def calculate_vwap(self, ticker: str, start: str, end: str):
+        ticker_data = yf.Ticker(self.ticker).history(start = self.start, end = self.end)
+        avg_price = (ticker_data['Open'] + ticker_data['High'] + ticker_data['Low'] + ticker_data['Close']) / 4
+        vwap = sum(avg_price * ticker_data['Volume']) / sum(ticker_data['Volume'])
+        return vwap
 
+    # Calculate Relative Strength Index
+    def get_RSI(self, ticker: str, start: str, end: str):
+        ticker_data = yf.Ticker(self.ticker).history(start = self.start, end = self.end)
+        ticker_data['price_diff'] = ticker_data['Close'] - ticker_data['Open']
+        up_closes = []
+        down_closes = []
+        for index, row in ticker_data.iterrows():
+            if row['price_diff'] > 0:
+                up_closes.append(row['Close'])
+            else:
+                down_closes.append(row['Close'])
+        avg_up_close = sum(up_closes) / len(up_closes)
+        avg_down_close = sum(down_closes) / len(down_closes)
+        relative_strength = avg_up_close / avg_down_close
+        return (100 - (100 / (1 + relative_strength)))
     
